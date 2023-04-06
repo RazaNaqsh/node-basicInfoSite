bf2513617185cb28008fc08671587cdde9104fd8
RazaNaqsh <owaisraza1704@gmail.com>
2023-04-06 15:22:07 +0530

    easier code

--- index.js
+++ index.js
@@ -1,30 +1,35 @@
+// create a server,
+// check req.url and according to conditon
+// readfile,
+// write res head, and res data
+// lastly listen on any port, with callback loggin
 const http = require("http");
 const fs = require("fs");
 
-const server = http.createServer((req, res) => {
-	const filePath = req.url === "/" ? "./index.html" : `.${req.url}.html`;
-	let contentType = "text/html";
-
-	fs.readFile(filePath, (error, content) => {
-		if (error) {
-			if (error.code == "ENOENT") {
-				filePath = "./error.html";
-				fs.readFile(filePath, function (error, content) {
-					res.writeHead(404, { "Content-Type": contentType });
-					res.end(content, "utf-8");
-				});
-			} else {
-				res.writeHead(500);
-				res.end(`Server error: ${error.code}`);
-			}
+http
+	.createServer((req, res) => {
+		if (req.url === "/") {
+			fs.readFile("index.html", (err, content) => {
+				res.write(content);
+				return res.end();
+			});
+		} else if (req.url === "/about") {
+			fs.readFile("about.html", (err, content) => {
+				res.write(content);
+				return res.end();
+			});
+		} else if (req.url === "/contact") {
+			fs.readFile("contact.html", (err, content) => {
+				res.write(content);
+				return res.end();
+			});
 		} else {
-			res.writeHead(200, { "Content-Type": contentType });
-			res.end(content, "utf-8");
+			fs.readFile("error.html", (err, content) => {
+				res.write(content);
+				return res.end();
+			});
 		}
+	})
+	.listen(1704, () => {
+		console.log("listening on port 1704");
 	});
-});
-
-const port = 8080;
-server.listen(port, () => {
-	console.log(`Server running on port ${port}`);
-});
