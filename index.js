2c58219feac7952bced62a0c60d05c7eb44d2bcb
RazaNaqsh <owaisraza1704@gmail.com>
2023-04-06 15:35:42 +0530

    Add res head and error handle

--- index.js
+++ index.js
@@ -1,8 +1,3 @@
-// create a server,
-// check req.url and according to conditon
-// readfile,
-// write res head, and res data
-// lastly listen on any port, with callback loggin
 const http = require("http");
 const fs = require("fs");
 
@@ -10,21 +5,29 @@ http
 	.createServer((req, res) => {
 		if (req.url === "/") {
 			fs.readFile("index.html", (err, content) => {
+				if (err) throw err;
+				res.writeHead(200, { "Content-Type": "text/html" });
 				res.write(content);
 				return res.end();
 			});
 		} else if (req.url === "/about") {
 			fs.readFile("about.html", (err, content) => {
+				if (err) throw err;
+				res.writeHead(200, { "Content-Type": "text/html" });
 				res.write(content);
 				return res.end();
 			});
 		} else if (req.url === "/contact") {
 			fs.readFile("contact.html", (err, content) => {
+				if (err) throw err;
+				res.writeHead(200, { "Content-Type": "text/html" });
 				res.write(content);
 				return res.end();
 			});
 		} else {
 			fs.readFile("error.html", (err, content) => {
+				if (err) throw err;
+				res.writeHead(200, { "Content-Type": "text/html" });
 				res.write(content);
 				return res.end();
 			});
